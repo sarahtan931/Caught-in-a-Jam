8f0601061d45a8dd46402d83117339dca82e31d7
sarahtan931 <sarah.tan931@gmail.com>
2020-03-08 11:53:48 -0400

    PlayerMovement
    Made the player move smoothly around the world

--- Caught in a Jam/Assets/_Scripts/PlayerController.cs
+++ Caught in a Jam/Assets/_Scripts/PlayerController.cs
@@ -13,29 +13,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.y++;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.y--;
-            this.transform.position = position;
-        }
+
+        /*   Vector3 move = Vector3.zero;
+           float speed = 1f;
+
+           if (Input.GetKeyDown(KeyCode.LeftArrow))
+           {
+               move = Vector3.left;
+           }
+           if (Input.GetKeyDown(KeyCode.RightArrow))
+           {
+               move = Vector3.right;
+           }
+           if (Input.GetKeyDown(KeyCode.UpArrow))
+           {
+               move = Vector3.up;
+           }
+           if (Input.GetKeyDown(KeyCode.DownArrow))
+           {
+               move = Vector3.down;
+           }
+
+           gameObject.transform.position = gameObject.transform.position + move * speed;
+           */
+        float speed = 5f;
+        //Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        //change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
     }
 }
