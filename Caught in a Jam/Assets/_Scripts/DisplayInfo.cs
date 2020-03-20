e51249d7cdd25459cd27c1cd6da653a322b07b25
Anusha <asamara5@uwo.ca>
2020-03-19 21:23:59 -0400

    Health stats

--- Caught in a Jam/Assets/_Scripts/DisplayInfo.cs
+++ Caught in a Jam/Assets/_Scripts/DisplayInfo.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class DisplayInfo : MonoBehaviour
 {
+    private int healthTrack = PlayerMovement.health;
+    private Text healthText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthText = this.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      healthTrack = PlayerMovement.health;
+      healthText.text = "Health: " + healthTrack + "/5";
     }
 }
