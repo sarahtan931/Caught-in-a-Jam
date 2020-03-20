f778e64a8e2e233e969b795a6d667370d83598b5
Anusha <asamara5@uwo.ca>
2020-03-19 23:45:07 -0400

    Merge branch 'master' of https://github.com/sarahtan931/asamara5_lkamink_ssawan2_stan229_SE2250_RPG

--- Caught in a Jam/Assets/_Scripts/GetMainChar.cs
+++ Caught in a Jam/Assets/_Scripts/GetMainChar.cs
@@ -10,16 +10,10 @@ public class GetMainChar : MonoBehaviour
     private SpriteRenderer mySprite;
     private readonly string selectedCharacter = "SelectedCharacter";
 
-    private Text stats;
-
-    private int strength = 0;
-    private int defense = 0;
-    private float rot = 0;
-
     private void Awake()
     {
         mySprite = this.GetComponent<SpriteRenderer>();
-        stats = this.GetComponent<Text>();
+       
 
     }
     // Start is called before the first frame update
@@ -33,21 +27,12 @@ public class GetMainChar : MonoBehaviour
         {
             case 1:
                 mySprite.sprite = peach;
-                strength = 2;
-                defense = 4;
-                rot = 1.05f;
                 break;
             case 2:
                 mySprite.sprite = apple;
-                strength = 4;
-                defense = 2;
-                rot = 1.25f;
                 break;
             case 3:
                 mySprite.sprite = strawberry;
-                strength = 3;
-                defense = 2;
-                rot = 1f;
                 break;
             case 4:
                 mySprite.sprite = blueberry;
@@ -60,6 +45,6 @@ public class GetMainChar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        stats.text = "Strength: " + strength + "/10, Defense: " + defense + "/10";
+
     }
 }
