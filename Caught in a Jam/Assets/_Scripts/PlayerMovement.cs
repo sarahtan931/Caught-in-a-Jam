3cb94ab4aa4957c79d583fab425e82a7cf0bcb46
lkaminker <60855519+lkaminker@users.noreply.github.com>
2020-03-20 00:55:38 -0400

    Jumping Combat

--- Caught in a Jam/Assets/_Scripts/PlayerMovement.cs
+++ Caught in a Jam/Assets/_Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public float projectileSpeed = 40;
     private bool direction;
     private bool isOnGround = false;
+    private Rigidbody2D combatJump;
 
     public static int health = 5;
 
@@ -20,90 +21,109 @@ public class PlayerMovement : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         direction = false;
+
+        combatJump = this.GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Grass")
+        if (collision.gameObject.tag == "Grass")
         {
             isOnGround = true;
         }
 
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
         {
-            health--;
+         foreach (ContactPoint2D point in collision.contacts)
+             if (point.normal.y >= 0.9f) { }
+            else { health--; }
         }
 
         if (health <= 0)
         {
             Destroy(this.gameObject);
         }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            foreach (ContactPoint2D point in collision.contacts)
+            {
+                if (point.normal.y >= 0.9f)
+                {
+                    Vector2 velocity = combatJump.velocity;
+                    velocity.y = jumpSpeed;
+                    combatJump.velocity = velocity;
+                    Destroy(collision.collider.gameObject);
+                }
+            }
+        }
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        movement = Input.GetAxis("Horizontal");
-        if (movement > 0f)
+        // Update is called once per frame
+        void Update()
         {
-            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
-            direction = true;
+            movement = Input.GetAxis("Horizontal");
+            if (movement > 0f)
+            {
+                rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+                direction = true;
+            }
+
+            else if (movement < 0f)
+            {
+                rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+                direction = false;
+            }
+
+            else
+            {
+                rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround == true)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+                isOnGround = false;
+            }
+
+            //allow the ship to fire 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TempFire();
+            }
         }
 
-        else if (movement < 0f)
+        void TempFire()
         {
-            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
-            direction = false;
+            GameObject projGo = Instantiate<GameObject>(projectilePrefab);
+            projGo.transform.position = transform.position;
+            Rigidbody2D rigidB = projGo.GetComponent<Rigidbody2D>();
+            //rojectile testProj = projGo.GetComponent<Projectile>();
+
+            if (direction == true)
+            {
+                rigidB.velocity = Vector2.right * projectileSpeed;
+            }
+            else
+            {
+                rigidB.velocity = Vector2.left * projectileSpeed;
+            }
         }
 
-        else
+        // references the last GameObject that triggered Hero's collider
+        void OnTriggerEnter2D(Collider2D collider)
         {
-            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
-        }
+            Transform rootT = collider.gameObject.transform.root;
+            GameObject go = rootT.gameObject;
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround == true)
-        {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
-            isOnGround = false;
-        }
+            // destroys the enemy the GameObject collides with the enemy (object with tag "Enemy")
+            if (go.tag == "pickUp")
+            {
 
-        //allow the ship to fire 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TempFire();
+                Destroy(go);
+            }
+
+
         }
     }
-
-    void TempFire()
-    {
-        GameObject projGo = Instantiate<GameObject>(projectilePrefab);
-        projGo.transform.position = transform.position;
-        Rigidbody2D rigidB = projGo.GetComponent<Rigidbody2D>();
-       //rojectile testProj = projGo.GetComponent<Projectile>();
-
-       if (direction == true)
-       {
-            rigidB.velocity = Vector2.right * projectileSpeed;
-        }
-       else
-       {
-            rigidB.velocity = Vector2.left * projectileSpeed;
-        }
-    }
-
-    // references the last GameObject that triggered Hero's collider
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        Transform rootT = collision.gameObject.transform.root;
-        GameObject go = rootT.gameObject;
-       
-        // destroys the enemy the GameObject collides with the enemy (object with tag "Enemy")
-        if (go.tag == "pickUp")
-        {
-   
-            Destroy(go);
-        }
-
-        
-    }
-}
