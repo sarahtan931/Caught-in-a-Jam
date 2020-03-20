4792009199d6d604f2bc955dce56ff651ef5cf8d
Anusha <asamara5@uwo.ca>
2020-03-20 09:09:01 -0400

    Fixing stick

--- Caught in a Jam/Assets/_Scripts/Stick.cs
+++ Caught in a Jam/Assets/_Scripts/Stick.cs
@@ -16,42 +16,44 @@ public class Stick : MonoBehaviour
         stickDirection = PlayerMovement.direction;
         player = this.transform.parent.gameObject;
 
-        if (PlayerPrefs.GetInt(selectedCharacter) != 4)
+        if (PlayerPrefs.GetInt(selectedCharacter) != 2)
         {
             stick.SetActive(false);
         }
 
+    }
 
-        // Update is called once per frame
-        void Update()
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        stickDirection = PlayerMovement.direction;
+
+        if (stickDirection == true)
         {
-            if (stick.activeSelf)
+            this.gameObject.transform.position = new Vector2(6 + player.transform.position.x, -2 + player.transform.position.y);
+        }
+
+        if (stickDirection == false)
+        {
+            this.gameObject.transform.position = new Vector2(-6 + player.transform.position.x, -2 + player.transform.position.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (stickDirection == true)
             {
-                stickDirection = PlayerMovement.direction;
+                this.gameObject.transform.position = new Vector2(12 + player.transform.position.x, -2 + player.transform.position.y);
+            }
 
-                if (stickDirection == true)
-                {
-                    this.gameObject.transform.position = new Vector2(6 + player.transform.position.x, -2 + player.transform.position.y);
-                }
-
-                if (stickDirection == false)
-                {
-                    this.gameObject.transform.position = new Vector2(-6 + player.transform.position.x, -2 + player.transform.position.y);
-                }
-
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    if (stickDirection == true)
-                    {
-                        this.gameObject.transform.position = new Vector2(12 + player.transform.position.x, -2 + player.transform.position.y);
-                    }
-
-                    if (stickDirection == false)
-                    {
-                        this.gameObject.transform.position = new Vector2(-12 + player.transform.position.x, -2 + player.transform.position.y);
-                    }
-                }
+            if (stickDirection == false)
+            {
+                this.gameObject.transform.position = new Vector2(-12 + player.transform.position.x, -2 + player.transform.position.y);
             }
         }
+
     }
 }
+    
+
