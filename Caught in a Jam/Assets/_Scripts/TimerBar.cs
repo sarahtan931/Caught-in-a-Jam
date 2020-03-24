33013be2943b829fad4eb47ab9aaa21dc5890c31
lkaminker <60855519+lkaminker@users.noreply.github.com>
2020-03-24 16:53:01 -0400

    Timer Bar

---
+++ Caught in a Jam/Assets/_Scripts/TimerBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerBar : MonoBehaviour
+{
+    public GameObject timesUpText;
+    public float livesLeft = 3f;
+
+    public float startingTime = 100.0f;
+    public Slider Timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timesUpText.SetActive(false);
+        Timer = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        startingTime -= Time.deltaTime;
+        Timer.value = startingTime;
+
+        if (startingTime <= 0)
+        {
+            timesUpText.SetActive(true);
+            startingTime = 0;
+            livesLeft--;
+            RestartAsync();
+        }
+    }
+    // Pause before restarting game
+    async Task RestartAsync()
+    {
+        await Task.Delay(2000);
+        if (livesLeft > 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        } else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    }
