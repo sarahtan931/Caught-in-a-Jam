be329ce6ba7047249d02b30a186d355f96931008
ssawan2 <ssawan2@uwo.ca>
2020-04-03 01:34:11 -0400

    comment

--- Caught in a Jam/Assets/_Scripts/BoundsCheck.cs
+++ Caught in a Jam/Assets/_Scripts/BoundsCheck.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
-
+     
     [Header("Set in Inspector")]
     public float radius = 1f;
     public bool keepOnScreen = true;
--- Caught in a Jam/Assets/_Scripts/TextControl.cs
+++ Caught in a Jam/Assets/_Scripts/TextControl.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class TextControl : MonoBehaviour
 {
+    //Displaying the stats to the User 
     public Text textBox;
 
     // Update is called once per frame
@@ -13,17 +14,19 @@ public class TextControl : MonoBehaviour
 
         if (textBox.gameObject.tag == "Speed")
         {
+            //printing the speed 
             print(textBox.gameObject.tag);
             textBox.text = "Speed: " + PlayerMovement.speed;
         }
 
+        //printing the strength 
         if (textBox.gameObject.tag == "Strength")
         {
             print(textBox.gameObject.tag);
             textBox.text = "Strength: " + PlayerMovement.strength;
         }
-
-        if(textBox.gameObject.tag == "Points")
+        //printing the points 
+        if (textBox.gameObject.tag == "Points")
         {
             textBox.text = "Score: " + StatsSave.S.points;
         }
