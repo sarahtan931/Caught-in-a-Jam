91c39a5a206dc010f9267b0aa4f5cdfea8aaf3ad
Anusha <asamara5@uwo.ca>
2020-04-03 00:28:47 -0400

    Stats

---
+++ Caught in a Jam/Assets/TextControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextControl : MonoBehaviour
+{
+    public Text textBox;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (textBox.gameObject.tag == "Speed")
+        {
+            print(textBox.gameObject.tag);
+            textBox.text = "Speed: " + PlayerMovement.speed;
+        }
+
+        if (textBox.gameObject.tag == "Strength")
+        {
+            print(textBox.gameObject.tag);
+            textBox.text = "Strength: " + PlayerMovement.strength;
+        }
+    }
+}
