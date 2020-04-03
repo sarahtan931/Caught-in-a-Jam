e49def26e5addbcbce499b868564e07e3d7a6de1
lkaminker <60855519+lkaminker@users.noreply.github.com>
2020-04-03 01:14:44 -0400

    comments

--- Caught in a Jam/Assets/_Scripts/Axe.cs
+++ Caught in a Jam/Assets/_Scripts/Axe.cs
@@ -18,24 +18,30 @@ public class Axe : MonoBehaviour
 
     private void Start()
     {
+        // assigns text to inactive so that the text appears only when it is triggered
         pickUpText.SetActive(false);
         doorText.SetActive(false);
+        // simulates the rigidbody for the axe so that it sits on the ground when it is not held
         item.GetComponent<Rigidbody2D>().simulated = true;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        // if player comes nearby, text appears explaining how to pick up the axe
         if (other.gameObject.CompareTag("Player"))
         {
             pickUpText.SetActive(true);
         }
 
+        // if the door entrapping the friend triggers the collider of the axe, the health of the door diminishes
+        // until the door is broken down
         if (other.gameObject.CompareTag("FriendDoor"))
         {
             doorText.SetActive(true);
             health--;
             if (health == 0)
             {
+                // destroys the door and displays text
                 Destroy(other.gameObject);
                 doorText.SetActive(false);
             }
@@ -44,9 +50,11 @@ public class Axe : MonoBehaviour
 
     void Update()
     {
+        // if the player is carrying the axe, it activates isKinematic to transfer the rigidbody effects to the script
         if (carrying)
         {
             Carry(item);
+            // player can only drop object if they are currently carrying it so carrying = true
             Drop();
         }
         else
@@ -61,8 +69,10 @@ public class Axe : MonoBehaviour
     }
     void PickUp()
     {
+        // if user presses down arrow, player will pick up axe
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            // turns gravity off and isKinematic to false to make it appear that the player is holding the axe
             carrying = true;
             item.GetComponent<Rigidbody2D>().gravityScale = 0;
             item.GetComponent<Rigidbody2D>().isKinematic = false;
@@ -75,10 +85,13 @@ public class Axe : MonoBehaviour
     // Update is called once per frame
     void Drop()
     {
+        // if user presses backspace key, axe will be placed back on the ground
         if (Input.GetKeyDown(KeyCode.Backspace))
         {
             carrying = false;
+            // turns gravity back on
             item.GetComponent<Rigidbody2D>().gravityScale = 3;
+            // makes axe static so player can pick up object in the same spot that it was dropped
             item.isStatic = true;
             item.transform.parent = null;
             item.transform.position = guide.transform.position;
